039b4be9e3ee709cee2f8d279ef5c9be0e69b39a
jeodom17 <jeodom17@gmail.com>
2021-10-25 10:39:19 -0400

    add routes to server.js file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.port || 3017;
 
 const app = express();
 
@@ -11,14 +11,13 @@ const app = express();
 //* Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 //* access to files in public folder
 app.use(express.static('public'));
 
 //* GET Route for homepage
 app.get('*', (req, res) =>
-  res.readFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 //* GET Route for  notes section
@@ -26,15 +25,33 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
+//todo: -----------------------------------------------------
 //* API routes ---- 'GET /api/notes' -> read db.json -> return all saved notes as JSON
 
+app.get('/api/notes', (req, res) => {
+  //* Inform the client
+  res.json(`${req.method} request received to get notes`);
+
+  //* Log our request to the terminal
+  console.info(`${req.method} request received to get notes`);
+});
 
 
+
+
+//todo: --------------------------------------------------------------------
 //* API routes ---- `POST /api/notes` -> receive new note to save on request body -> add to db.json -> return new note to client ++ give each note unique id when saved
 
+app.post('/api/notes', (req, res) => {
+  //* Inform the client that their POST request was received
+  res.json(`${req.method} request received to add a note`);
 
-//* listen to port 3001
+  //* Log our request to the terminal
+  console.info(`${req.method} request received to add a note`);
+});
+
+
+//* listen to port 3017
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
