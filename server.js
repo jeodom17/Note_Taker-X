6cfaebf8b434f4260487d95266eba146f031ab35
jeodom17 <jeodom17@gmail.com>
2021-10-26 20:40:16 -0400

    added api routes

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 //* access to files in public folder
 app.use(express.static('public'));
 
+
+//=============================================================================
+//* HTML Routes
 //* GET Route for homepage
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -25,12 +28,20 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-//todo: -----------------------------------------------------
+
+//=====================================================================================
 //* API routes ---- 'GET /api/notes' -> read db.json -> return all saved notes as JSON
 
 app.get('/api/notes', (req, res) => {
-  //* Inform the client
-  res.json(`${req.method} request received to get notes`);
+  //* 
+  fs.readFile('.db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return
+    } else {
+      res.send(data);
+    }
+  })
 
   //* Log our request to the terminal
   console.info(`${req.method} request received to get notes`);
@@ -38,20 +49,83 @@ app.get('/api/notes', (req, res) => {
 
 
 
-
-//todo: --------------------------------------------------------------------
 //* API routes ---- `POST /api/notes` -> receive new note to save on request body -> add to db.json -> return new note to client ++ give each note unique id when saved
 
 app.post('/api/notes', (req, res) => {
-  //* Inform the client that their POST request was received
-  res.json(`${req.method} request received to add a note`);
+  //* 
+  req.body.id = uuidv4();
 
-  //* Log our request to the terminal
-  console.info(`${req.method} request received to add a note`);
+  let savedNotes = fs.readFileSync('/db/db.json', 'utf8');
+  let jsonNotes = JSON.parse(savedNotes);
+  jsonNotes.push(req.body);
+  let newData = JSON.stringify(jsonNotes, null, 2);
+  
+  fs.writeFile('./db/db.json', newData, (err) => {
+    if (err) {
+      res.send('SAVE NOTE FAILED:', err)
+
+    } else {
+      res.send(`NOTE SUCCESSFULLY SAVED: ID = ${req.body.id}`)
+    }
+    });
+  });
+
+
+//* edit notes with app.put ---- 
+
+app.put('/api/notes/:id', (req, res) => {
+  let editID = req.params.id;
+  let savedNotes = fs.readFileSync('/db/db.json', 'utf8');
+  let jsonNotes = JSON.parse(savedNotes);
+  let updateNotes = jsonNotes.reduce((arr, note) => {
+    if (note.id == editID) {
+      arr.push(req.body);
+    } else {
+      arr.push(note);
+    }
+    return arr;
+  }, []);
+
+  let newData = JSON.stringify(updateNotes, null, 2);
+
+  fs.writeFile('./db/db.json', newData, (err) => {
+    if (err) {
+      res.send('EDIT NOTE FAILED:', err)
+
+    } else {
+      res.send(`SUCCESSFULLY EDITED NOTE: ID = ${req.params.id}`)
+    }
+  });
 });
 
+//* delete note with app.delete ---- 
+
+app.delete('api/notes/:id', (req, res) => {
+  let deleteID = req.params.id;
+  let savedNotes = fs.readFileSync('/db/db.json', 'utf8');
+  let jsonNotes = JSON.parse(savedNotes);
+  let updateNotes = jsonNotes.reduce((arr, note) => {
+    if (note.id !== deleteID) {
+      arr.push(note)
+    }
+    return arr;
+}, []);
+
+let newData = JSON.stringify(updateNotes, null, 2);
+
+fs.writeFile('./db/db.json', newData, (err) => {
+  if (err) {
+    res.send('DELETE NOTE FAILED:', err)
+
+  } else {
+    res.send(`DELETED NOTE: ID = ${req.params.id}` )
+  }
+});
+
+})
 
 //* listen to port 3017
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
+
